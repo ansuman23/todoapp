449e57e365d8b4a840d82e312a5d52aeacb04b07
unknown <ansumanparija982@gmail.com>
2024-07-21 13:56:40 +0530

    modified utility function

--- src/utils/utility.js
+++ src/utils/utility.js
@@ -1,11 +1,3 @@
-// export function formatDate(dateString) {
-//     const date=new Date(dateString);
-//     const day=date.getDate();
-//     const month=date.toLocaleString('default', { month: 'long' });
-//     const year=date.getFullYear();
-//     const suffix=day%10===1 && day!==11 ?'st':day%10===2&&day!==12?'nd':day%10===3&&day!==13?'rd':'th';
-//     return `${day}${suffix} ${month} ${year}`;
-// }
 export function formatDate(dateString) {
     const [month,day,year]=dateString.split('/').map(Number);
     const date=new Date(year,month-1,day);
@@ -14,4 +6,10 @@ export function formatDate(dateString) {
     const yearNumber=date.getFullYear();
     const suffix=dayNumber % 10 === 1 && dayNumber !== 11?'st':dayNumber % 10 === 2 && dayNumber !== 12 ? 'nd':dayNumber % 10 === 3 && dayNumber !== 13?'rd': 'th';
     return `${dayNumber}${suffix} ${monthName} ${yearNumber}`;
+}
+export function formatTime(timeString) {
+    let [hours,minutes]=timeString.split(':').map(Number);
+    const period=hours >= 12 ?'PM':'AM';
+    hours=hours%12||12; 
+    return `${hours}:${minutes < 10 ? '0' : ''}${minutes} ${period}`;
 }
